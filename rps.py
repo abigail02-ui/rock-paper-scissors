d7638282314b7ef418eb98051fcaca5f0a33681e
Abigail T J <232070740+abigail02-ui@users.noreply.github.com>
2025-09-22 23:18:25 +0530

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,11 +1,4 @@
-"""
-Create a Rock Paper Scissors game where the player inputs their choice
-and plays  against a computer that randomly selects its move, 
-with the game showing who won each round.
-Add a score counter that tracks player and computer wins, 
-and allow the game to continue until the player types “quit”.
-"""
-
+#to play stone papr scissor with computer 
 import random
 import tkinter as tk
 from tkinter import messagebox
